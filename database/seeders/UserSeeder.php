9504dc07116184e22a893ca7d78b19ecaa2db4f7
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-08-16 22:44:39 +0700

    add: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'remember_token' => Str::random(60),
+            'password' => Hash::make('password123'),
+        ]);
+    }
+}

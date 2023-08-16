1e106106d18d1b1f3136c2359c8a77cd9e5f08d8
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-08-17 00:50:33 +0700

    test: register auth test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -20,22 +20,67 @@ class AuthTest extends TestCase
         ]);
 
         $response->assertStatus(201)
-            ->assertJsonStructure([
-                'response',
-                'success',
-                'message',
-                'data'
+            ->assertJson([
+                'response' => '201',
+                'success' => true,
+                'message' => 'Register successfully.',
+                'data' => [
+                    'name' => 'John Doe',
+                    'email' => 'john@example.com',
+                    'password' => 'password123',
+                ]
             ]);
 
     }
 
     public function testRegisterFailed()
     {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => '',
+            'email' => '',
+            'password' => '',
+        ]);
+
+        $response->assertStatus(400)
+            ->assertJson([
+                'response' => '400',
+                'success' => false,
+                'message' => [
+                    'name' => [
+                      'The name field is required.'
+                    ],
+                    'email' => [
+                        'The email field is required.'
+                    ],
+                    'password' => [
+                        'The password field is required.'
+                    ]
+                ],
+            ]);
 
     }
 
     public function testRegisterEmailAlreadyExists()
     {
-        
+        $existingUser = \App\Models\User::factory()->create([
+            'email' => 'john@example.com',
+        ]);
+
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'password123',
+        ]);
+
+        $response->assertStatus(400)
+            ->assertJson([
+                'response' => '400',
+                'success' => false,
+                'message' => [
+                    'email' => [
+                        'The email has already been taken.'
+                    ]
+                ],
+            ]);
     }
 }
